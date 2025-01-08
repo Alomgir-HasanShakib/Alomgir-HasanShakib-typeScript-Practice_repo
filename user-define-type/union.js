4dc1eb82af2d1ef098676e369e4bcfc9d84a7671
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-08 22:25:43 +0600

    compile union.ts file

---
+++ user-define-type/union.js
@@ -0,0 +1,13 @@
+var userId;
+userId = 1234;
+// userId = "ahkh12"; // Here user id only can be number . There is no way to store userId as a string . so here give a error
+// But this example store idNumber as number and string in a same type. And this is called `UNION Type`
+var idNumber;
+idNumber = 1234;
+idNumber = "ahkh12";
+// this is a example for function
+function myfn(studentId) {
+    console.log(studentId);
+}
+myfn("ahdk12");
+myfn(1234);
