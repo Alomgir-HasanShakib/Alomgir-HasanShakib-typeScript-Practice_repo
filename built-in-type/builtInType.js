4482b30621a35318ef265f89ae6aa6b91e004443
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-08 22:14:09 +0600

    install typescritp understand built in data type and practice it

---
+++ built-in-type/builtInType.js
@@ -0,0 +1,7 @@
+var userName;
+userName = "Shakib";
+// userName = 123; // There is a error becouse userName cannot received number type couse i define userName as a string
+var number;
+number = 12345687;
+// number = "1234"; // There is a error becouse number cannot received other data type except number type
+// There is more built in data type such as undefined, null, void, boolean
---
+++ built-in-type/builtInType.ts
@@ -0,0 +1,9 @@
+let userName: string;
+userName = "Shakib";
+// userName = 123; // There is a error becouse userName cannot received number type couse i define userName as a string
+
+let number: number;
+number = 12345687;
+// number = "1234"; // There is a error becouse number cannot received other data type except number type
+
+// There is more built in data type such as undefined, null, void, boolean
