b9bfea09a67a923b419101a2a0d761271f807c76
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-09 19:13:21 +0600

    knowing any type in typescript and write it a bit

---
+++ user-define-type/any.js
@@ -0,0 +1,7 @@
+// Any type -> if you dont have no idea about the data type then use any type.
+var user;
+var users;
+user = "shakib";
+user = 2;
+user = undefined;
+user = true;
---
+++ user-define-type/any.ts
@@ -0,0 +1,9 @@
+// Any type -> if you dont have no idea about the data type then use any type.
+
+let user: any;
+let users: any[];
+
+user = "shakib";
+user = 2;
+user = undefined;
+user = true;
