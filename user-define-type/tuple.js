aab205f1b39f66d1770278c57436d17f9c541485
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-09 18:52:28 +0600

    understand tuple data type and practice it

---
+++ user-define-type/tuple.js
@@ -0,0 +1,5 @@
+// mixide data
+var user;
+user = [101, "shakib"];
+user.push(102, "shanto");
+console.log(user);
---
+++ user-define-type/tuple.ts
@@ -0,0 +1,8 @@
+// mixide data
+let user: [number, string];
+
+user = [101, "shakib"];
+
+user.push(102, "shanto");
+
+console.log(user);
