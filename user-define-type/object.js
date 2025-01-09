16b39b00d89617c958c6bffd2e18d26c36774c57
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-01-09 19:28:04 +0600

    express object type in typescript

---
+++ user-define-type/object.js
@@ -0,0 +1,6 @@
+//  Object type -> for storing object, kay value pair data
+// let users: object;
+var users;
+users = { userName: "shakib", userId: 1, isActive: true };
+// for storing array of object
+var developers;
---
+++ user-define-type/object.ts
@@ -0,0 +1,8 @@
+//  Object type -> for storing object, kay value pair data
+
+// let users: object;
+let users: { userName: string; userId: number; isActive: boolean };
+users = { userName: "shakib", userId: 1, isActive: true };
+
+// for storing array of object
+let developers: object[];
